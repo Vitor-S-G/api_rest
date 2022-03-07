7045ebbdd2a7da545cb3e2a582d065dac3236440
Vitor <vitorsoaresv17@gmail.com>
2022-03-07 12:02:32 -0300

    Adicionando cors

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ dotenv.config();
 
 import './database';
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
@@ -20,6 +22,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors());
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use('/images', express.static(resolve(__dirname, '..', 'uploads', 'images')));
